a946d2436c70eb9afae88867fef2cd138531dbc8
Jussel Elejorde <20-06232@g.batstate-u.edu.ph>
2024-04-16 09:19:15 +0800

    pat

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import {TypeAnimation} from 'react-type-animation'
+import {TypeAnimation} from 'react-type-animation';
 import DownloadButton from "./DownloadButton";
-
 
 const Main = () => {
 
     const fileName = "Resume"; // Define the file name here
     return (
         <div id="main">
-           <img className="w-full h-screen object-cover" src="/src/assets/samuele-errico-piccarini-FMbWFDiVRPs-unsplash.jpg" alt="" />
+           <img className="w-full h-screen object-cover" src="personal-portfolio/src/assets/samuele-errico-piccarini-FMbWFDiVRPs-unsplash.jpg" alt="" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/0">  
        
             <div className="flex justify-end ">
@@ -17,7 +16,7 @@ const Main = () => {
             </div>
         
 
-            <div className="max-w-[700px] m-auto h-full w-full flex flex-col content-start pt-52">
+            <div className="max-w-[600px] m-auto h-full w-full flex flex-col content-start pt-12">
                 <h1 className="text-white text-9xl font-bold">I'm Jussel</h1>
                 <h2 className="text-white flex text-2xl">I'm a 
                 <TypeAnimation
